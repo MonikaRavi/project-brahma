94969340776e2e17e9348143810ef0814b326fb2
Monika Ravi <monikar@hawsco.com>
2018-03-22 09:26:26 -0700

    Added server.js, Procfile and modified Package.json => postinstall, start and added angular-cli to Dependency

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
-var server_port = process.env.YOUR_PORT || process.env.PORT || 80;
-var server_host = process.env.YOUR_HOST || '0.0.0.0';
-server.listen(server_port, server_host, function() {
-    console.log('Listening on port %d', server_port);
-});
+const express = require('express');
+const app = express();
+// Run the app by serving the static files
+// in the dist directory
+app.use(express.static(__dirname + '/dist'));
+// Start the app by listening on the default
+// Heroku port
+app.listen(process.env.PORT || 8080);
