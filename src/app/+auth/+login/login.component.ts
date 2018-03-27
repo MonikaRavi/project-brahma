619f4e3a956daf1058d038538f11ddf1db4711b7
Monika Ravi <monikar@hawsco.com>
2018-03-27 11:42:33 -0700

    Fade out for Home page

--- src/app/+auth/+login/login.component.ts
+++ src/app/+auth/+login/login.component.ts
@@ -14,7 +14,7 @@ export class LoginComponent implements OnInit {
 
   login(event){
     event.preventDefault();
-    this.router.navigate(['/home'])
+    this.router.navigate(['/home/home'])
   }
 
 }
